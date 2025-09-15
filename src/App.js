c6d04a84ced9a1f1486ad022a825f69d90501e1b
Dmitry Kavin <kavin.dmt@mail.ru>
2025-09-15 21:40:14 +0300

    Дз8_

--- src/App.js
+++ src/App.js
@@ -1,53 +1,33 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import NewsSearch from './components/NewsSearch';
-import NewsList from './components/NewsList';
+import React, { useState, useCallback } from 'react';
+import TodoList from './components/TodoList';
+import AddTodoModal from './components/AddTodoModal';
 import './App.css';
 
-const API_KEY = '623f36ca-1fab-4908-adf0-37321113063b';
-
 function App() {
-    const [articles, setArticles] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [query, setQuery] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    
+    const handleAddTodo = useCallback(
+        (newTodo) => {
+            setTodos((prevTodos) => [...prevTodos, newTodo]);
+        },
+        [setTodos]
+    );
 
-    const fetchNews = async (searchQuery, page = 1) => {
-        try {
-            const response = await axios.get(
-                `https://content.guardianapis.com/search?q=${searchQuery}&page=${page}
-&api-key=${API_KEY}`
-            );
-
-            setArticles(response.data.response.results);
-            setQuery(searchQuery);
-            setCurrentPage(page);
-        } catch (error) {
-            console.error('Ошибка при загрузке новостей:', error);
-        }
-    };
-
-    const handlePageChange = (direction) => {
-        const nextPage = currentPage + direction;
-        if (nextPage > 0) {
-            fetchNews(query, nextPage);
-        }
-    };
+    const openModal = useCallback(() => setIsModalOpen(true), []);
+    const closeModal = useCallback(() => setIsModalOpen(false), []);
 
     return (
         <div className="app">
-            <h1>Новости The Guardian</h1>
-            <NewsSearch onSearch={(query) => fetchNews(query)} />
-            <NewsList articles={articles} />
-            {articles.length > 0 && (
-                <div className="pagination">
-                    <button onClick={() => handlePageChange(-1)}>Предыдущая</button>
-                    <span>Страница: {currentPage}</span>
-                    <button onClick={() => handlePageChange(1)}>Следующая</button>
-                </div>
-            )}
+            <h1>Список задач</h1>
+            <button className="add-todo-button" onClick={openModal}>
+                Добавить задачу
+            </button>
+            <TodoList todos={todos} />
+            {isModalOpen && <AddTodoModal onAdd={handleAddTodo}
+onClose={closeModal} />}
         </div>
     );
-
 }
 
 export default App;
